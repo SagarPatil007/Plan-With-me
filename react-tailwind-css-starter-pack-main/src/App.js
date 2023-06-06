26271ba4fc710a5a04e7a13e2548379c8a583f16
sagar patil <sagarpatil@gmail.com>
2023-06-06 14:24:56 +0530

    first commit

---
+++ react-tailwind-css-starter-pack-main/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+import data from './data';
+import Tours from "./Components/Tours";
+
+const App = ()=>{
+
+  // get the data using usestate
+  const [tours,settours] = useState(data);
+
+  function removeTour(id){
+      const newTours = tours.filter(tour => tour.id !== id); 
+      settours(newTours);
+  }
+
+  
+  if(tours.length === 0){
+    return(
+        <div className="refresh">
+            <h2>No Tours Left</h2>
+            <button className="btn-white" onClick={()=> settours(data)}>Refresh</button>
+        </div>
+    )
+   }
+
+  return (
+    <div className="App">
+      <Tours tours={tours} removeTour={removeTour}></Tours>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ react-tailwind-css-starter-pack-main/src/Components/Tours.js
@@ -0,0 +1,21 @@
+
+import Card from "./Card";
+
+function Tours({tours,removeTour}){
+    return(
+        <div className="container">
+            <div>
+                <h2 className="title">Plan With Sagar</h2>
+            </div>
+            <div className="cards">
+                {
+                    tours.map((tours)=>{
+                        return <Card {...tours} removeTour={removeTour}></Card>
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Tours;
